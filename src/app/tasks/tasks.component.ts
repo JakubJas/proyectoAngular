0ed8cff87db574e9f96db0fdecc9f1ff10e9557e
JakubJ2001 <jjasinski1234@gmail.com>
2024-09-03 13:05:41 +0100

    Poner en funcionamineto el boton de añadir tarea

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -12,6 +13,7 @@ export class TasksComponent {
 
   @Input({required: true}) userId?: String
   @Input({required: true}) name?: String
+  isAddingTask = false;
 
   dummyTasks = [
     {
@@ -45,5 +47,9 @@ export class TasksComponent {
 
   onCompleteTask(id: string){
     this.dummyTasks = this.dummyTasks.filter((task) => task.id !== id); 
+  }
+
+  onAddTask(){
+    this.isAddingTask = true;
   }
 }
